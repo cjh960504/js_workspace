25fabf894c86885ac8dcc6f0d6d91d3f48d71a54
DESKTOP-HEE5N61\cjh96 <cjh960504@gmail.com>
2020-09-24 23:26:10 +0900

    17일차 javascript

---
+++ project0924/nation.js
@@ -0,0 +1,15 @@
+var data =[
+    ["../../images/nation/1.png","세계 최대 강대국","미국"],
+    ["../../images/nation/2.jpg","산초","멕시코"],
+    ["../../images/nation/3.jpg","로마제국","그리스"],
+    ["../../images/nation/4.png","신사의 나라","영국"],
+    ["../../images/nation/5.jpg","나찌","독일"],
+    ["../../images/nation/6.png","무적함대","스페인"],
+    ["../../images/nation/7.jpg","에펠탑","프랑스"],
+    ["../../images/nation/8.gif","차이코프스키","러시아"],
+    ["../../images/nation/9.png","쌈바축구","브라질"],
+    ["../../images/nation/10.jpg","북아메리카 대륙","캐나다"],
+    ["../../images/nation/11.jpg","후지산, 원숭이","일본"],
+    ["../../images/nation/12.png","쿵푸 팬더","중국"],
+    ["../../images/nation/13.gif","코알라,캥거루","호주"]
+];
